225a8784b6e82b7a6838e505ca0bc9ed2c4b42c3
grant956 <68757554+studennikovIv@users.noreply.github.com>
2022-12-04 23:10:28 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import  App  from 'components/App';
-import API from './components/API/API'
+import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App API={API}/>
+    <App />
   </React.StrictMode>
 );
